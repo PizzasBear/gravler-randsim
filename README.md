d9fa7f64e14232a3b1a63edf3ae37132d78be57f
max <gmx.sht@gmail.com>
2024-08-11 22:39:02 +0300

    2nd submission (bit manipulation 18.2s)
    Signed-off-by: max <gmx.sht@gmail.com>

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # Rust implementation for Gravler Randsim
+
 Using multithreading.
 
-My output using an Ryzen 5900x
+My output on my desktop (Ryzen 5900x)
+
 ```
 Thread 0 with 83333333 rounds
-0,6: best=64
-0,16: best=64
-0,41: best=71
+0,32: best=70
+0,77: best=72
+0,81: best=72
+0,172: best=73
+0,185: best=73
+0,290: best=73
+0,570: best=74
+0,806: best=74
 Thread 1 with 83333333 rounds
+1,293: best=76
+0,1404: best=76
 Thread 2 with 83333333 rounds
 Thread 3 with 83333333 rounds
 Thread 4 with 83333333 rounds
@@ -18,22 +27,28 @@ Thread 8 with 83333334 rounds
 Thread 9 with 83333334 rounds
 Thread 10 with 83333334 rounds
 Thread 11 with 83333334 rounds
-1,1472: best=84
-4,19104: best=89
-5,565635: best=92
-5,3395182: best=95
-3,16756699: best=96
-Thread 5 done
+1,43891: best=90
+7,71221: best=90
+6,138424: best=90
+11,306843: best=91
+8,574159: best=91
+3,1668520: best=96
+4,3527477: best=96
+9,10083038: best=97
+3,49307374: best=98
+1,69370728: best=98
 Thread 1 done
+Thread 9 done
 Thread 6 done
 Thread 8 done
-Thread 7 done
-Thread 9 done
-Thread 10 done
-Thread 0 done
 Thread 11 done
-Thread 3 done
-Thread 4 done
 Thread 2 done
-DONE! elapsed=29.259886111s
+Thread 10 done
+Thread 3 done
+Thread 7 done
+Thread 4 done
+Thread 0 done
+Thread 5 done
+DONE! elapsed=18.239890172s
+
 ```
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,33 @@ use std::{
     time::Instant,
 };
 
-const NUM_THREADS: usize = 12;
 const TOTAL_ROUNDS: usize = 1000_000_000;
+
+/// Takes 64-bit random number representing 32 numbers between 0 and 3 (inclusive)
+/// and returns the number of 3s in the sequence.
+fn count_hits(rand64: u64) -> u32 {
+    // 8 bit example:
+    //
+    //   00011011  (0, 1, 2, 3 represented as 2 bits each)
+    //    00011011 (number shifted right)
+    // & 01010101  (0x55)
+    //   --------
+    //   00000001  -> 1 hit
+
+    (rand64 >> 1 & rand64 & 0x5555_5555_5555_5555).count_ones()
+}
 
 fn thread_main(t: usize, thread_rounds: usize, best: &AtomicU32) {
     println!("Thread {t} with {thread_rounds} rounds");
     let mut rng = SmallRng::from_entropy();
     for gen in 0..thread_rounds {
-        let mut hits = 0;
-        for _ in 0..231 {
-            hits += (rng.gen_range(0..4u8) == 0) as u32;
-        }
+        // 64 * 8 / 2 = 256 numbers
+        // 256 - 231 = 25 numbers to remove
+
+        // sets the first 25 random numbers to 0, and counts only the last 7 numbers
+        let mut hits = count_hits(rng.gen::<u64>() >> 2 * 25);
+        // counts 32 * 7 = 224 numbers
+        hits += (0..7).map(|_| count_hits(rng.gen())).sum::<u32>();
 
         let best = best.fetch_max(hits, atomic::Ordering::SeqCst);
         if best == hits {
@@ -30,14 +46,16 @@ fn main() {
 
     let best = AtomicU32::new(0);
     thread::scope(|s| {
+        let num_threads = num_cpus::get_physical();
         let mut rounds_left = TOTAL_ROUNDS;
-        for t in 0..NUM_THREADS {
+        for t in 0..num_threads {
             let best = &best;
 
-            let thread_rounds = rounds_left / (NUM_THREADS - t);
+            let thread_rounds = rounds_left / (num_threads - t);
             rounds_left -= thread_rounds;
             s.spawn(move || thread_main(t, thread_rounds, best));
         }
+        assert_eq!(rounds_left, 0);
     });
 
     println!("DONE! elapsed={:?}", start.elapsed());
